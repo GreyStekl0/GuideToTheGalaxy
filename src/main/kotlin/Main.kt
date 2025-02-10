d3160791a85b9f23a35bf50a6d356317abc58fdc
Stekl0 <greystekl0@gmail.com>
2025-02-10 20:20:53 +0500

    Refactor quote element selector to use a variable for class name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,11 @@ import org.jsoup.Jsoup
 
 fun main() {
     val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
+    val teg = "sc-2aegk7-2 bzpNIu"
 
     val document = Jsoup.connect(url).get()
 
-    val quoteElements = document.select("div[class=sc-2aegk7-2 bzpNIu]")
+    val quoteElements = document.select("div[class=$teg]")
 
     for (element in quoteElements) {
         val quoteText = element.text()
