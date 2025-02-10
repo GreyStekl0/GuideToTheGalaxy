7119d38c40cd17466acb2c9ffb58b71a217ca323
Stekl0 <greystekl0@gmail.com>
2025-02-10 20:19:08 +0500

    Add Jsoup dependency and implement web scraping in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,14 @@
-package org.stekl0
+import org.jsoup.Jsoup
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    // TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
 
-    for (i in 1..5) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val document = Jsoup.connect(url).get()
+
+    val quoteElements = document.select("div[class=sc-2aegk7-2 bzpNIu]")
+
+    for (element in quoteElements) {
+        val quoteText = element.text()
+        println("$quoteText\n")
     }
 }
